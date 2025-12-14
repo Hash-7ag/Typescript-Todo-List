7592c9a638a0c28940fc9c5985d5e4fe65444daa
Nan0-S <nan.fgbs@gmail.com>
2025-12-14 12:18:02 +0400

    chore: Final settings 2 (add lucide-react)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <main>
-      
+    <main className='py-10 h-screen'>
+      <h1 className='font-bold text-3xl text-center'>Your Todos</h1>
     </main>
   )
 }
