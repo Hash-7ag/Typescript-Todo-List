2474a44bdf3a887f11985b1eb380cb17bb3de841
Nan0-S <nan.fgbs@gmail.com>
2025-12-15 18:23:14 +0400

    Unidirectional data flow explained, callback props

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { dummyData } from "./data/todos"
 
 function App() {
 
+   function setTodoCompleted(id: number, completed: boolean) {
+      alert(`Todo with id: ${id} is now ${completed ? "completed" : "cot completed"}`)
+   } 
+
   return (
     <main className='py-10 h-screen'>
       <h1 className='font-bold text-3xl text-center text-slate-900 pb-4'>Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 p-10 rounded-xl">
          <div className="space-y-2">
             {dummyData.map(todo=> (
-               <TodoItem todo={todo} />
+               <TodoItem key={todo.id} todo={todo} onCompletedChange={setTodoCompleted}/>
             ))}
          </div>
       </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,11 +2,12 @@ import type { Todo } from "../types/todo"
 
 interface TodoItemProps {
    todo: Todo;
+   onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-function TodoItem({todo}: TodoItemProps) {
+function TodoItem({todo, onCompletedChange}: TodoItemProps) {
    return (
-      <div key={todo.id}>
+      <div>
          <label className="
          flex items-center gap-2 
          border rounded-xl border-slate-700 
@@ -14,6 +15,8 @@ function TodoItem({todo}: TodoItemProps) {
          bg-white hover:bg-slate-200 hover:scale-[1.032] transition-all ">
             <input 
             type="checkbox" 
+            checked={todo.completed}
+            onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
             className="scale-125 hover:scale-[1.35] transition-all accent-slate-700"  
             />  
             <span className={todo.completed ? "line-through text-xl text-slate-400" : "text-xl text-slate-700"}>{todo.title}</span> 
